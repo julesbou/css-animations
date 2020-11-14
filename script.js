5aa05febef5c4642056289d6494efb5cefaaab14
Jules B <jules.bous@gmail.com>
2020-11-14 11:34:27 +0100

    'dot inversion' animation

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 
 const template = ({ title, uid, date, from, template, css, scss }, singlePage) => {
   return `
-    <article id="${uid}">
-      <h1 class="title"><a href="#${uid}">${title}</a></h1>
-      <span class="date">${new Date(date).toLocaleDateString()}</span>
-      <div class="anim">${template}</div>
+    <article id='${uid}'>
+      <h1 class='title'><a href='#${uid}'>${title}</a></h1>
+      <span class='date'>${new Date(date).toLocaleDateString()}</span>
+      <div class='anim'>${template}</div>
       <style>${css}</style>
-      <p class="notice">
-        <span class="from"><b>Inspired from:</b><br> <a href="${from}">${from}</a></span>
+      <p class='notice'>
+        <span class='from'><b>Inspired from:</b><br> <a href='${from}'>${from}</a></span>
       </p>
       <footer>
-        ${singlePage ? '' : `<p class="permalink"><a href="#${uid}">See alone</a></p>`}
-        <p class="viewsource"><a>SCSS code of animation</a></p>
+        ${singlePage ? '' : `<p class='permalink'><a href='#${uid}'>See alone</a></p>`}
+        <p class='viewsource'><a>SCSS code of animation</a></p>
       </footer>
-      <div class="source">
+      <div class='source'>
         <pre>${scss}</pre>
         <!--<pre>${template.replace(/&/g, '&amp;').replace(/</g, '&lt;')}</pre>-->
       </div>
@@ -42,6 +42,12 @@ function displayAnimations() {
     return a.date < b.date ? 1 : -1
   })
 
+  // remove animations not supported by current browser
+  const browser = browserName()
+  displayedAnimations = displayedAnimations.filter(anim => {
+    return !anim.disable || anim.disable.indexOf(browser) === -1
+  })
+
   // animation not found
   if (hash && !displayedAnimations.length) {
     console.error('animation not found')
@@ -60,6 +66,8 @@ function displayAnimations() {
       $list.insertAdjacentHTML('beforeend', template(animation, !!hash))
     })
 
+    setTimeout(() => document.body.classList.remove('standby'), 800)
+    
   }, animations.length === 1 ? 0 : 500)
 }
 
@@ -75,3 +83,17 @@ $main.addEventListener('click', function(event) {
 })
 
 window.onhashchange = () => displayAnimations()
+
+function browserName() {
+  const agent = window.navigator.userAgent.toLowerCase()
+  switch (true) {
+    case agent.indexOf('edge') > -1: return 'edge';
+    case agent.indexOf('edg') > -1: return 'chrome'; // chromium based edge
+    case agent.indexOf('opr') > -1 && !!window.opr: return 'opera';
+    case agent.indexOf('chrome') > -1 && !!window.chrome: return 'chrome';
+    case agent.indexOf('trident') > -1: return 'ie';
+    case agent.indexOf('firefox') > -1: return 'firefox';
+    case agent.indexOf('safari') > -1: return 'safari';
+    default: return 'other';
+  }
+}
